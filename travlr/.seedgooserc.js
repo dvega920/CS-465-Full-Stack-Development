65a95714e792bc7862e5e4b96329da91b18d67da
David Vega <dvega920@gmail.com>
2023-10-13 18:36:36 -0400

    moved proj files out of travlr directory and deleted travlr directory

--- travlr/.seedgooserc.js
+++
@@ -1,7 +0,0 @@
-// .seedgooserc.js
-module.exports = {
-    modelBaseDirectory: 'app_api/database/models', // model directory name
-    models: ['**/*.js', '!db.js'], // model matcher
-    data: 'data', // data directory name
-    db: 'mongodb+srv://admin:admin@cluster1.l3r7pjc.mongodb.net/' // db connection url
-};
